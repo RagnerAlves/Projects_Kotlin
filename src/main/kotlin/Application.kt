a2c1179c1cb8f6175fe78e4abdb02fad7a183e1b
Ragner Alves <ragnerf@hotmail.com>
2024-02-14 14:57:15 -0300

    exer_update

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,10 +1,15 @@
-/*fun main() {
-    println("Hello Word")
-}*/
+package org.example
+fun main() {
+    //println("Hello Word")
 
-val i: Int = 6
+    //val i: Int = 6
 
-val b1 = i.toByte()
-println(b1)
+    //val b1 = i.toByte()
+    //println(b1)
+
+    val numberOfFish = 5
+    val numberOfPlants = 12
+    "I have $numberOfFish fish" + " and $numberOfPlants plants"
+}
 
 
